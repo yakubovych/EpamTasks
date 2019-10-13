528ccb8d7828f72b0329eef6990a99ed7ba1dfbc
Roman Yakubovych <yakubovychr@gmail.com>
2019-10-13 19:04:03 +0300

    add unit tests for first task

---
+++ OnlinePart/HW1.NetTests/RectangleTask1Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HW.Net.Tests
+{
+    public class RectangleTask1Tests
+    {
+        [TestMethod()]
+        public void Perimetеr_5and5_20returned()
+        {
+            double LeftTopY = 5;
+            double LeftTopX = 10;
+            double RightBottomY = 10;
+            double RightBottomX = 15;
+
+            double expected = 20;
+
+            RectangleTask1 rectangle = new RectangleTask1(LeftTopY, LeftTopX, RightBottomY, RightBottomX);
+            double actual = rectangle.Perimetеr();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
